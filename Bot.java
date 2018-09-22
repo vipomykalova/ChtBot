bcfd583f46a0d21325e93c2f243e45b1a5de41c3
vipomykalova <vipomykalova@mail.ru>
2018-09-23 02:55:20 +0500

    update

---
+++ Bot.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Bot {
+
+	public static void main(String[] args) {
+		
+		Dialog curDialog = new Dialog();
+		System.out.println(curDialog.sendStartMessage());
+		Scanner in = new Scanner(System.in);
+		
+		while(true) {
+			String request = in.nextLine();
+			
+			if (request.trim().toLowerCase().equals("о себе")) {
+				System.out.println(curDialog.sendStartMessage());
+				
+			} else if (request.trim().endsWith("да")) {
+				Game currentClient = new Game();
+				currentClient.GameLogic();
+				if (currentClient.result == true) {
+					System.out.println(curDialog.sendWinnerMessage());
+				}
+				else System.out.println(curDialog.sendLoserMessage());
+				System.out.println(curDialog.sendRepeatedStartMessage());
+				
+			} else if (request.trim().endsWith("нет")) {
+				System.out.println(curDialog.sendFarewellMessage());
+				
+			} else System.out.println(curDialog.sendStartMessage());
+		}
+		
+	}
+}
