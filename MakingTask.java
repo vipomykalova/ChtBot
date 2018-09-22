5453f538bd986e01b222ef7af79d21202e6132a6
vipomykalova <vipomykalova@mail.ru>
2018-09-23 02:53:26 +0500

    nothing changes

---
+++ MakingTask.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Random;
+
+public class MakingTask {
+	
+	public static String newTask() {
+		
+		Random rnd = new Random();
+		int thisWord = rnd.nextInt(2);
+		try(BufferedReader br = new BufferedReader(new FileReader("Archive.txt")))
+		{
+		    String s;
+		    int count = 0;
+		    while((s = br.readLine()) != null) {
+		    	if(count == thisWord)
+		    		return s;
+		        count ++;
+		    }
+		}
+		catch(IOException ex) {
+			return null;
+		}
+		return null; 
+	}
+	
+}
