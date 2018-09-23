7175b5fed1aff76b73ff3a05a5e19748f4ea4540
Grisslow <43199047+Grisslow@users.noreply.github.com>
2018-09-23 23:36:06 +0500

    without static

--- LifeCounter.java
+++ LifeCounter.java
@@ -1,9 +1,9 @@
 
 public class LifeCounter {
 	
-	public static int lifes = 10;
+	public int lifes = 10;
 	
-	public static int lifeCounter(boolean resultOfStroke) {
+	public int lifeCounter(boolean resultOfStroke) {
 		if (!resultOfStroke) lifes -= 1;
 		return lifes;
 	}
