68d057c6ea7f87d1a12a0a60c2e6d93853549e62
vipomykalova <vipomykalova@mail.ru>
2018-09-29 14:26:14 +0500

    Change everything. Used ResourceBundle.

--- Dialog.java
+++ Dialog.java
@@ -1,37 +1,19 @@
 
-public class Dialog {
+import java.util.ListResourceBundle;
 
+public class Dialog extends ListResourceBundle{
+	
+	static final Object[][] contents = {
+			{"привет", "Привет :) Я бот, который умеет "
+					+ "играть в \"Виселицу\". Поиграем? Напиши \"да\" или \"нет\"."},
+			{"победа", "Поздравляю, ты победил!"},
+			{"проигрыш", "Упс, ты проиграл :("},
+			{"еще", "Сыграем еще?"},
+			{"пока", "Обидно! Но если вдруг захочешь, то пиши мне :) Я буду ждать!"},
+		    };
+	
+	public Object[][] getContents() {
+		return contents;
+	    }
 
-	private final String START_MESSAGE = "Привет :) Я бот, который умеет "
-			+ "играть в \"Виселицу\". Поиграем? Напиши \"да\" или \"нет\".";
-
-	private final String WINNER_MESSAGE = "Поздравляю, ты победил!";
-
-	private final String LOSER_MESSAGE = "Упс, ты проиграл :(";
-
-	private final String REPEATED_MESSAGE = "Сыграем еще?";
-
-	private final String FAREWELL_MESSAGE = "Обидно! Но если вдруг захочешь,"
-			+ " то пиши мне :) Я буду ждать!";
-	
-	public String sendStartMessage() {
-		return START_MESSAGE;
-	}
-	
-	public String sendWinnerMessage() {
-		return  WINNER_MESSAGE;
-	}
-	
-	public String sendLoserMessage() {
-		return  LOSER_MESSAGE;
-	}
-	
-	public String sendRepeatedStartMessage() {
-		return REPEATED_MESSAGE;
-	}
-	
-	public String sendFarewellMessage() {
-		return FAREWELL_MESSAGE;
-	}
-	
 }
