5e069ca2a4c764202fcbeb12896ae0bfe5176f51
vipomykalova <vipomykalova@mail.ru>
2018-10-01 07:49:55 +0500

    Без использования вечного цикла.

--- Hangman.java
+++ Hangman.java
@@ -16,51 +16,53 @@ public class Hangman extends InputOutput{
 		}
 		
 		printResult(word.length(), currentResult);
+		win = false;
+		boolean finish = false;
 		
-		while(true) {
-			if(life.lifes == 0) {
-				win = false;
-				output("Слово: " + word + "\n");
-				life.lifes = 10;
-				break;
-			}
-			
-			boolean finish = false;
+		while(life.IsHeAlive()) {
 			String letter = input();
+			if(letter.isEmpty()) {
+				continue;
+			}
 			
 			boolean isItRightLetter = false;
 			ArrayList<Integer> indexes = new ArrayList<Integer>();
+			
 			for(int i = 0; i < word.length(); i++) {
-				if(wordToArray[i] == (letter.charAt(0))) {
-					isItRightLetter = true;
-					indexes.add(i);
-				}
+ 				if(wordToArray[i] == (letter.charAt(0))) {
+ 					isItRightLetter = true;
+ 					indexes.add(i);
+ 				}
 			}
 			
 			if(isItRightLetter) {
-				life.lifes = life.lifeCounter(true);
+				life.lives = life.lifeCounter(true);
 				for(int i = 0; i < indexes.size(); i++) {
 					currentResult[indexes.get(i)] = wordToArray[indexes.get(i)];
 					wordToArray[indexes.get(i)] = '-';
 				}
-			} else life.lifes = life.lifeCounter(false);
+			} else life.lives = life.lifeCounter(false);
 			
-			output("У вас осталось жизней: " + life.lifes + "\n");
+			output("У вас осталось жизней: " + life.lives + "\n");
 			
-			printResult(word.length(), currentResult);
+			if (life.lives > 0) {
+				printResult(word.length(), currentResult);
+			}
 			
 			int count = 0;
 			for(int i = 0; i < currentResult.length; i++) {
-				if(currentResult[i] != '-') count++;
-			}
-			if(count == currentResult.length) finish = true;
+ 				if(currentResult[i] != '-') count++;
+ 			}
+ 			if(count == currentResult.length) finish = true;
 			
 			if(finish) {
 				win = true;
-				life.lifes = 10;
+				life.lives = 10;
 				break;
 			}
-			
+		}
+		if (!finish) {
+			output("Верное слово: "+ word + "\n");
 		}
 	}
 	
