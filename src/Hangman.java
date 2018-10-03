718c323bc3f7ae6a00f60ec4da8eea2fa4b0c002
Grisslow <grislow@icloud.com>
2018-10-03 20:15:08 +0500

    проверка буквы через словарь

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Hangman {
 	
@@ -6,7 +8,7 @@ public class Hangman {
 	
 	public void game(){
 		String word = TaskMaker.newTask();
-		char wordToArray[] = word.toCharArray();
+		Map<Character, ArrayList<Integer>> wordsLetters = wordToDict(word);
 		char currentResult[] = new char[word.length()];
 		LifeCounter life = new LifeCounter();
 		
@@ -20,46 +22,50 @@ public class Hangman {
 		
 		while(life.IsHeAlive() || finish) {
 			String letter = InOut.INSTANCE.input();
+
 			if(letter.isEmpty()) {
-				continue;
-			}
-			
-			boolean isItRightLetter = false;
-			ArrayList<Integer> indexes = new ArrayList<Integer>();
-			
-			for(int i = 0; i < word.length(); i++) {
- 				if(wordToArray[i] == (letter.charAt(0))) {
- 					isItRightLetter = true;
- 					indexes.add(i);
- 				}
-			}
-			
-			if(isItRightLetter) {
+				InOut.INSTANCE.output("Попробуй еще! Я верю в тебя ;) \n");
+			} else if(wordsLetters.containsKey(letter.charAt(0))) {
 				life.lives = life.lifeCounter(true);
-				for(int i = 0; i < indexes.size(); i++) {
-					currentResult[indexes.get(i)] = wordToArray[indexes.get(i)];
-					wordToArray[indexes.get(i)] = '-';
+
+				for(int i = 0; i < wordsLetters.get(letter.charAt(0)).size(); i++) {
+					currentResult[wordsLetters.get(letter.charAt(0)).get(i)] = letter.charAt(0);
 				}
 			} else life.lives = life.lifeCounter(false);
-			
+
 			InOut.INSTANCE.output("У вас осталось жизней: " + life.lives + "\n");
-			
-			if (life.lives > 0) {
+
+			if(life.lives > 0) {
 				printResult(word.length(), currentResult);
 			}
-			
+
 			int count = 0;
 			for(int i = 0; i < currentResult.length; i++) {
- 				if(currentResult[i] != '-') count++;
- 			}
- 			if(count == currentResult.length) finish = true;
+				if(currentResult[i] != '-') count++;
+			}
+			if(count == currentResult.length) finish = true;
 		}
+
 		if (!finish) {
 			InOut.INSTANCE.output("Верное слово: "+ word + "\n");
 		} else {
 			win = true;
 			life.lives = 10;
 		}
+	}
+
+	private Map<Character, ArrayList<Integer>> wordToDict(String word) {
+		Map<Character, ArrayList<Integer>> dict = new HashMap<Character, ArrayList<Integer>>();
+
+		for(int i = 0; i < word.length(); i++) {
+			ArrayList<Integer> indexes = new ArrayList<Integer>();
+			for(int j = i; j < word.length(); j++) {
+				if(word.charAt(j) == word.charAt(i))
+					indexes.add(j);
+			}
+			dict.putIfAbsent(word.charAt(i), indexes);
+		}
+		return dict;
 	}
 	
 	private void printResult(int n, char[] result) {
