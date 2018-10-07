5b9669e32c36ee7d84e5b565426b341772b10e5c
vipomykalova <vipomykalova@mail.ru>
2018-10-07 16:11:14 +0500

    Добавила возможность работы КА с игрой Правда или действие

--- src/Brain.java
+++ src/Brain.java
@@ -3,7 +3,7 @@ public class Brain {
 	
 	FMS brain = new FMS();
 	String currentAnswer = "";
-	
+
 	Brain() {
 		brain.setState(this::letsPlay);
 	}
@@ -12,32 +12,74 @@ public class Brain {
 		InOut.INSTANCE.output(Dialog.INSTANCE.getString("привет"));
 		currentAnswer = InOut.INSTANCE.input();
 		switch(currentAnswer) {
-			case "нет":
-				brain.setState(this::sendByeMessage);
-				break;
-			case "да":
-				brain.setState(this::getWord);
-				break;
-			case "о себе":
-				brain.setState(this::letsPlay);
-				break;
+		    case "виселица":
+		        brain.setState(this::hangman);
+		        break;
+		    case "правда или действие":
+		        brain.setState(this::truthOrDare);
+		        break;
+		    case "о себе":
+		        brain.setState(this::letsPlay);
+		        break;
+		}
+	}
+	
+	public void truthOrDare() {
+		InOut.INSTANCE.output(Dialog.INSTANCE.getString("правда или действие"));
+		currentAnswer = InOut.INSTANCE.input();
+		switch(currentAnswer) {
+		    case "да":
+			    brain.setState(this::truthOrDareGame);
+			    break;
+		    case "нет":
+			    brain.setState(this::sendByeMessage);
+			    break;
+		    case "о себе":
+		    	brain.setState(this::letsPlay);
+		    	break;
+		}
+	}
+	
+	public void truthOrDareGame() {
+		TruthOrDare currentClient = new TruthOrDare();
+		currentClient.game();
+		
+		if (currentClient.endOfGame) {
+			InOut.INSTANCE.output(Dialog.INSTANCE.getString("конец"));}
+		
+		brain.setState(this::sendByeMessage);
+	}
+	
+	public void hangman() {
+		InOut.INSTANCE.output(Dialog.INSTANCE.getString("виселица"));
+		currentAnswer = InOut.INSTANCE.input();
+		switch(currentAnswer) {
+		    case "да":
+			    brain.setState(this::hangmanGame);
+			    break;
+		    case "нет":
+			    brain.setState(this::sendByeMessage);
+			    break;
+		    case "о себе":
+			    brain.setState(this::letsPlay);
+			    break;
 		}
 	}
 
-	public void getWord() {
+	public void hangmanGame() {
 		Hangman currentClient = new Hangman();
 		currentClient.game();
 
 		if (currentClient.win) {
 			InOut.INSTANCE.output(Dialog.INSTANCE.getString("победа"));
 		}
-		if (!currentClient.win) {
+		else {
 			InOut.INSTANCE.output(Dialog.INSTANCE.getString("проигрыш"));
 		}
-		brain.setState(this::doYouWant);
+		brain.setState(this::doYouWantInHangman);
 	}
 	
-	public void doYouWant() {
+	public void doYouWantInHangman() {
 		InOut.INSTANCE.output(Dialog.INSTANCE.getString("еще"));
 		currentAnswer = InOut.INSTANCE.input();
 		switch(currentAnswer) {
@@ -45,7 +87,7 @@ public class Brain {
 				brain.setState(this::sendByeMessage);
 				break;
 			case "да":
-				brain.setState(this::getWord);
+				brain.setState(this::hangmanGame);
 				break;
 			case "о себе":
 				brain.setState(this::letsPlay);
