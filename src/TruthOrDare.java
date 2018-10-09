09daba63f0564a2b11d7e492c096945cb26e9fe5
vipomykalova <vipomykalova@mail.ru>
2018-10-09 22:09:56 +0500

    логика построения игры строится на входной строке

--- src/TruthOrDare.java
+++ src/TruthOrDare.java
@@ -1,15 +1,14 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 public class TruthOrDare {
 
-	private enum StatesGame {
+	public enum StatesGame {
 		Correct, Incorrect, Stop
 	}
 	public StatesGame currentStateGame;
-	private HashMap<String, String> nameArchive;
-	String[] gamers;
+	public HashMap<String, String> nameArchive;
+	public String[] gamers;
 	
 	TruthOrDare() {
 		nameArchive = new HashMap<>();
@@ -17,24 +16,34 @@ public class TruthOrDare {
 		nameArchive.put("действие", "Dare");
 	}
 
-	public String taskforPlayer(String answer){
-		if(answer.equals("стоп")) {
-			currentStateGame = StatesGame.Stop;
-			return "";
+	public String taskForPlayer(String answer){
+		checkState(answer);
+		switch(currentStateGame) {
+		case Correct:
+			return TaskMaker.newTask(nameArchive.get(answer)) + "\n";
+		case Incorrect:
+			return null;
+		case Stop:
+			return null;
 		}
-		if (!answer.equals("правда") && !answer.equals("действие")) {
-			currentStateGame = StatesGame.Incorrect;
-			return "";
-		} else {
+		
+		return null;
+	}
+	
+	public void checkState(String answer) {
+		if (answer.equals("стоп")) {
+			currentStateGame = StatesGame.Stop;
+		} else if (answer.equals("правда") || answer.equals("действие")) {
 			currentStateGame = StatesGame.Correct;
-			return TaskMaker.newTask(nameArchive.get(answer));
+		} else {
+			currentStateGame = StatesGame.Incorrect;
 		}
 	}
 
 	public String askPlayer() {
 		Random rnd = new Random();
 		String currentGamer = gamers[rnd.nextInt(gamers.length)];
-		return currentGamer + ", правда или действие? :)";
+		return currentGamer + ", правда или действие? :)" + "\n";
 	}
 
 	public void parseNames(String names) {
