97e8ad3a764f9aa0836add68bef6df16b903e141
vipomykalova <vipomykalova@mail.ru>
2018-10-09 23:21:37 +0500

    создаем экземпляр класса для ввода/вывода

--- src/Application.java
+++ src/Application.java
@@ -3,11 +3,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		Brain brain = new Brain();
+		InOutConsole console = new InOutConsole();
 
 		while(true) {
-			String userInput = InOut.INSTANCE.input();
-			String response = brain.update(userInput);
-			InOut.INSTANCE.output(response);
+			String userInput = console.input();
+			String response = brain.reply(userInput);
+			console.output(response);
 		}
 	}
 }
