3815289ff13cf9126d6b881b31b30d65430af75c
vipomykalova <vipomykalova@mail.ru>
2018-10-09 22:13:32 +0500

    логика перестроена по типу "получил сообщение - ответь"

--- src/Brain.java
+++ src/Brain.java
@@ -1,115 +1,119 @@
 
 public class Brain {
 	
-	FMS brain = new FMS();
+	FMS fsm = new FMS();
 	String currentAnswer = "";
+	Hangman currentHangman;
+	TruthOrDare currentTruthOrDare;
 
 	Brain() {
-		brain.setState(this::letsPlay);
+		fsm.setState(this::startMessage);
 	}
 	
-	public void letsPlay() {
-		InOut.INSTANCE.output(Dialog.INSTANCE.getString("привет"));
-		currentAnswer = InOut.INSTANCE.input();
-		switch(currentAnswer) {
-		    case "виселица":
-		        brain.setState(this::hangman);
-		        break;
-		    case "правда или действие":
-		        brain.setState(this::truthOrDare);
-		        break;
-		    case "о себе":
-		        brain.setState(this::letsPlay);
-		        break;
+	public String startMessage(String input) {
+		fsm.setState(this::gameSelection);
+		return Dialog.INSTANCE.getString("привет");
+	}
+	
+	public String gameSelection(String input) {
+		switch(input) {
+		case "виселица":
+			fsm.setState(this::hangmanWordGeneration);
+			return Dialog.INSTANCE.getString("начало");
+		case "правда или действие":
+			fsm.setState(this::truthOrDareGetNames);
+			return Dialog.INSTANCE.getString("начало");	
+		case "о себе":
+			fsm.setState(this::gameSelection);
+			return Dialog.INSTANCE.getString("привет");
 		}
+		fsm.setState(this::gameSelection);
+		return Dialog.INSTANCE.getString("некорректный ввод");
 	}
 	
-	public void truthOrDare() {
-		InOut.INSTANCE.output(Dialog.INSTANCE.getString("правда или действие"));
-		currentAnswer = InOut.INSTANCE.input();
-		switch(currentAnswer) {
-		    case "да":
-			    brain.setState(this::truthOrDareGame);
-			    break;
-		    case "нет":
-			    brain.setState(this::sendByeMessage);
-			    break;
-		    case "о себе":
-		    	brain.setState(this::letsPlay);
-		    	break;
-		}
+	public String hangmanWordGeneration(String input) {
+		currentHangman = new Hangman();
+		fsm.setState(this::hangmanGame);
+		return currentHangman.setWord();
 	}
 	
-	public void truthOrDareGame() {
-		TruthOrDare currentClient = new TruthOrDare();
-		currentClient.game();
+	public String hangmanGame(String input) {    
+		String result = currentHangman.currentResult(input);
 		
-		if (currentClient.endOfGame) {
-			InOut.INSTANCE.output(Dialog.INSTANCE.getString("конец"));}
+		switch(currentHangman.currentStateGame) {
+		case Win:
+			fsm.setState(this::wantMore);
+			return result;
+		case Fail:
+			fsm.setState(this::wantMore);
+			return result;
+		case Game:
+			fsm.setState(this::hangmanGame);
+			return result;
+		case Stop:
+		    fsm.setState(this::startMessage);
+		    return Dialog.INSTANCE.getString("пока"); 
+		}	
+		return null;
+	}
+	
+	public String wantMore(String input) {
+		switch(input) {
+		case "да":
+			fsm.setState(this::hangmanWordGeneration);
+			return Dialog.INSTANCE.getString("начало");	
+		case "нет":
+			fsm.setState(this::startMessage);
+			return Dialog.INSTANCE.getString("пока");
+		case "о себе":
+			fsm.setState(this::gameSelection);
+			return Dialog.INSTANCE.getString("привет");		
+		}
+		fsm.setState(this::wantMore);
+		return Dialog.INSTANCE.getString("некорректный ввод");
+	}
+	
+	public String truthOrDareGetNames(String input) {
+		fsm.setState(this::truthOrDareParseNames);
+		return Dialog.INSTANCE.getString("игроки");
+	}
+	
+	public String truthOrDareParseNames(String input) {
+		currentTruthOrDare = new TruthOrDare();
+		currentTruthOrDare.parseNames(input);
+		fsm.setState(this::truthOrDareGame);
+		return currentTruthOrDare.askPlayer();
+	}
+	
+	public String truthOrDareAskPlayer(String input) {
+		currentTruthOrDare.checkState(input);
+		if (currentTruthOrDare.currentStateGame == TruthOrDare.StatesGame.Stop) {
+			fsm.setState(this::startMessage);
+			return Dialog.INSTANCE.getString("пока");
+		}
+		fsm.setState(this::truthOrDareGame);
+		return currentTruthOrDare.askPlayer();
+	}
+	
+	public String truthOrDareGame(String input) {
+		String result = currentTruthOrDare.taskForPlayer(input);
 		
-		brain.setState(this::sendByeMessage);
+		switch(currentTruthOrDare.currentStateGame) {
+		case Correct:
+			fsm.setState(this::truthOrDareAskPlayer);
+			return result;
+		case Incorrect:
+			fsm.setState(this::truthOrDareGame);
+			return Dialog.INSTANCE.getString("некорректный ввод");
+		case Stop:
+			fsm.setState(this::startMessage);
+			return Dialog.INSTANCE.getString("пока");
+		}	
+		return null;
 	}
 	
-	public void hangman() {
-		InOut.INSTANCE.output(Dialog.INSTANCE.getString("виселица"));
-		currentAnswer = InOut.INSTANCE.input();
-		switch(currentAnswer) {
-		    case "да":
-			    brain.setState(this::hangmanGame);
-			    break;
-		    case "нет":
-			    brain.setState(this::sendByeMessage);
-			    break;
-		    case "о себе":
-			    brain.setState(this::letsPlay);
-			    break;
-		}
-	}
-
-	public void hangmanGame() {
-		Hangman currentClient = new Hangman();
-		currentClient.game();
-
-		if (currentClient.win) {
-			InOut.INSTANCE.output(Dialog.INSTANCE.getString("победа"));
-		}
-		else {
-			InOut.INSTANCE.output(Dialog.INSTANCE.getString("проигрыш"));
-		}
-		brain.setState(this::doYouWantInHangman);
-	}
-	
-	public void doYouWantInHangman() {
-		InOut.INSTANCE.output(Dialog.INSTANCE.getString("еще"));
-		currentAnswer = InOut.INSTANCE.input();
-		switch(currentAnswer) {
-			case "нет":
-				brain.setState(this::sendByeMessage);
-				break;
-			case "да":
-				brain.setState(this::hangmanGame);
-				break;
-			case "о себе":
-				brain.setState(this::letsPlay);
-				break;
-		}
-	}
-	
-	public void waitSmth() {
-		currentAnswer = InOut.INSTANCE.input();
-		if (!currentAnswer.isEmpty()) {
-			brain.setState(this::letsPlay);
-		}
-		else throw new IllegalStateException();
-	}
-	
-	public void sendByeMessage() {
-		InOut.INSTANCE.output(Dialog.INSTANCE.getString("пока"));
-		brain.setState(this::waitSmth);
-	}
-	
-	public void update() {
-		brain.update();
+	public String update(String input) {
+		return fsm.update(input);
 	}
 
 }
