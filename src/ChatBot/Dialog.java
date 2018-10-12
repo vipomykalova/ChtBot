3662dce932e22dabc2b93e2caabaebb71ae010a0
Grisslow <43199047+Grisslow@users.noreply.github.com>
2018-10-12 21:38:00 +0500

    все остальное в общей папке

---
+++ src/ChatBot/Dialog.java
@@ -0,0 +1,22 @@
+
+import java.util.ListResourceBundle;
+
+public class Dialog extends ListResourceBundle{
+
+	public static final Dialog INSTANCE = new Dialog();
+	
+	static final Object[][] contents = {
+			{"приветствие", "Привет :) Я бот, который умеет "
+					+ "играть в \"Виселицу\" и в \"Правду или действие\"."
+					+ " Во что хочешь поиграть?" + "\n"},
+			{"прощание", "Обидно! Но если вдруг захочешь, то пиши мне :) Я буду ждать!" + "\n"},
+			{"игроки", "Введи имена игроков (Напр.: Вика,Катя,Дима)" + "\n"},
+			{"некорректный ввод", "Ничего не понял, повтори :)" + "\n"},
+			{"начало", "Ну что ж, начинаем?" + "\n"},
+		    };
+	
+	public Object[][] getContents() {
+		return contents;
+	    }
+
+}
