2a86cda875c2c4fa072d29d56d622aef499c80b2
Grisslow <43199047+Grisslow@users.noreply.github.com>
2018-10-19 17:29:54 +0500

    многопользовательность

--- src/ChatBot/Application.java
+++ src/ChatBot/Application.java
@@ -1,14 +1,21 @@
 package ChatBot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
 
 	public static void main(String[] args) {
-		Brain brain = new Brain();
+		Map<String, Brain> players = new HashMap<String, Brain>();
+		//Map<String, String> games = new HashMap<String, String>();
+		//Brain brain = new Brain();
 		InOutConsole console = new InOutConsole();
 
 		while(true) {
 			String userInput = console.input();
-			String response = brain.reply(userInput);
+			String[] input = userInput.split(":");
+			players.putIfAbsent(input[0], new Brain());
+			String response = players.get(input[0]).reply(input[1]);
 			console.output(response);
 		}
 	}
