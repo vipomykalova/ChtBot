456c567dc7888f6a4fe4bc19f244abd3a44f784c
vipomykalova <vipomykalova@mail.ru>
2018-10-20 23:20:52 +0500

    Обработала некорректный ввод

--- src/ChatBot/Application.java
+++ src/ChatBot/Application.java
@@ -7,16 +7,20 @@ public class Application {
 
 	public static void main(String[] args) {
 		Map<String, Brain> players = new HashMap<String, Brain>();
-		//Map<String, String> games = new HashMap<String, String>();
-		//Brain brain = new Brain();
 		InOutConsole console = new InOutConsole();
 
 		while(true) {
-			String userInput = console.input();
-			String[] input = userInput.split(":");
-			players.putIfAbsent(input[0], new Brain());
-			String response = players.get(input[0]).reply(input[1]);
-			console.output(response);
+			try {
+				String userInput = console.input();
+				String[] input = userInput.split(":");
+				players.putIfAbsent(input[0], new Brain());
+				String response = players.get(input[0]).reply(input[1]);
+				console.output(response);
+			}
+			catch (ArrayIndexOutOfBoundsException exp){
+				console.output("Некорректный ввод. Примеры: \"Вика:виселица\","
+						+ " \"1:правда или действие\" \n");
+			}
 		}
 	}
 }
