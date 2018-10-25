ec4c1ae39375960a18a56219595117393f6917ee
vipomykalova <vipomykalova@mail.ru>
2018-10-25 18:26:21 +0500

    Использую эмодзи

--- src/ChatBot/Dialog.java
+++ src/ChatBot/Dialog.java
@@ -1,27 +1,29 @@
 package ChatBot;
 
 import java.util.ListResourceBundle;
+import com.vdurmont.emoji.EmojiParser;
 
 public class Dialog extends ListResourceBundle{
 
 	public static final Dialog INSTANCE = new Dialog();
 	
 	static final Object[][] contents = {
-			{"приветствие", "Привет :) Я бот, который умеет "
+			{"приветствие", EmojiParser.parseToUnicode("Привет :blush: Я бот, который умеет "
 					+ "играть в \"Виселицу\" и в \"Правду или действие\"."
-					+ " Во что хочешь поиграть? \n"},
-			{"прощание", "Обидно! Но если вдруг захочешь, то пиши мне :) Я буду ждать! \n"},
-			{"игроки", "Введи имена игроков (Напр.: Вика,Катя,Дима) \n"},
-			{"некорректный ввод", "Ничего не понял, повтори :) \n"},
-			{"начало", "Ну что ж, начинаем? \n"},
-			{"победа", "Молодец! Сыграем еще? \n"},
-			{"проигрыш", "Ты проиграл :( \n"},
-			{"еще", "Сыграем еще? \n"},
-			{"жизни", "У вас осталось жизней: "},
-			{"слово", "Загаданное слово: "},
-			{"что из", ", правда или действие? :) \n"},
-			{"пример ввода", "Некорректный ввод. Примеры: \"Вика:виселица\","
-					+ " \"1:правда или действие\" + \n"},
+					+ " Во что хочешь поиграть?:sparkles: \n")},
+			{"прощание", EmojiParser.parseToUnicode("Обидно! Но если вдруг захочешь, то пиши мне"
+					+ " :blush: Я буду ждать!:sparkles: \n")},
+			{"игроки", EmojiParser.parseToUnicode(":sparkles:Введи имена игроков (Напр.: Вика,Катя,Дима) \n")},
+			{"некорректный ввод", EmojiParser.parseToUnicode("Ничего не понял, повтори :think: \n")},
+			{"начало", EmojiParser.parseToUnicode("Ну что ж, начинаем?:sparkles: \n")},
+			{"победа", EmojiParser.parseToUnicode("Молодец!:tada: \nСыграем еще?:smirk: \n")},
+			{"проигрыш", EmojiParser.parseToUnicode("Ты проиграл :cry: \n")},
+			{"еще", EmojiParser.parseToUnicode("Сыграем еще?:smirk: \n")},
+			{"жизни", EmojiParser.parseToUnicode(":sparkles:Осталось жизней: ")},
+			{"слово", EmojiParser.parseToUnicode("Загаданное слово: \n")},
+			{"что из", EmojiParser.parseToUnicode(", правда или действие? :blush: \n")},
+			{"пример ввода", EmojiParser.parseToUnicode("Некорректный ввод:thumbsdown: Примеры: \"Вика:виселица\","
+					+ " \"1:правда или действие\":thumbsup: + \n")},
 	};
 	
 	public Object[][] getContents() {
