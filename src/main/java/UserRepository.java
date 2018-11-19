9a85ef20050802e627e52ced0ad6361a553cac89
vipomykalova <vipomykalova@mail.ru>
2018-11-19 23:38:34 +0500

    хранит главное поле - юзеры

--- src/main/java/UserRepository.java
+++ src/main/java/UserRepository.java
@@ -1,9 +1,10 @@
 package src.main.java;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentHashMap;;
 
 public class UserRepository {
 	
-	static Map<Long, Brain> users = new ConcurrentHashMap<Long, Brain>();
+	public static Map<Long, Brain> users = new ConcurrentHashMap<Long, Brain>();
+	
 }
